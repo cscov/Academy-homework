3ea613e714cf0778a162eb1a9aed216a4991c89c
cscov <carolyn.l.scoville@gmail.com>
2018-02-21 08:45:44 -0800

    Added partially completed RSpec homework

---
+++ W2D3/spec/dessert_spec.rb
@@ -0,0 +1,53 @@
+require 'rspec'
+require 'dessert'
+
+=begin
+Instructions: implement all of the pending specs (the `it` statements without blocks)! Be sure to look over the solutions when you're done.
+=end
+
+describe Dessert do
+  let(:chef) { double("chef") }
+  subject(:dessert) { Dessert.new('brownie', 200, 'Carolyn') }
+
+  describe "#initialize" do
+    it "sets a type" do
+      expect(dessert.type).to eq('brownie')
+    end
+
+    it "sets a quantity" do
+      expect(dessert.quantity).to eq(200)
+    end
+
+    it "starts ingredients as an empty array" do
+      expect(dessert.ingredients).to be_empty
+    end
+
+    it "raises an argument error when given a non-integer quantity" do
+      expect { Dessert.new('brownie', '200', 'Carolyn') }.to raise_error(ArgumentError)
+    end
+  end
+
+  describe "#add_ingredient" do
+    it "adds an ingredient to the ingredients array" do
+      # expect()
+    end
+  end
+
+  describe "#mix!" do
+    it "shuffles the ingredient array"
+  end
+
+  describe "#eat" do
+    it "subtracts an amount from the quantity"
+
+    it "raises an error if the amount is greater than the quantity"
+  end
+
+  describe "#serve" do
+    it "contains the titleized version of the chef's name"
+  end
+
+  describe "#make_more" do
+    it "calls bake on the dessert's chef with the dessert passed in"
+  end
+end
