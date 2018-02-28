7d42156146bb1f9f44ac02a49f0e2859d66c5813
cscov <carolyn.l.scoville@gmail.com>
2018-02-28 07:12:26 -0800

    Committing updates to W3D2 homework and adding W3D3 homework

--- W3D2/plays.rb
+++ W3D2/plays.rb
@@ -94,6 +94,36 @@ class Playwrights
   end
 
   def initialize(options)
-    
+    @id = options['id']
+    @name = options['name']
+    @birth_year = options['birth_year']
+  end
+
+  def create
+    raise "#{self} already in database" if @id
+    PlayDBConnection.instance.execute(<<-SQL, @name, @birth_year)
+      INSERT INTO
+        playwrights (name, birth_year)
+      VALUES
+        (?, ?)
+    SQL
+    @id = PlayDBConnection.instance.last_insert_row_id
+  end
+
+  def update
+    raise "#{self} not in database" unless @id
+    PlayDBConnection.instance.execute(<<-SQL, @name, @birth_year, @id)
+      UPDATE
+        playwrights
+      SET
+        name = ?, birth_year = ?
+      WHERE
+        id = ?
+    SQL
+  end
+
+  def get_plays
+    raise "#{self} not in database" unless @id
+    Play.find_by_playwright(@name)
   end
 end
