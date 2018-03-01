4f4060f06563951ca88badc8e4fb1d83536ee298
cscov <carolyn.l.scoville@gmail.com>
2018-03-01 07:24:06 -0800

    committing homework files

---
+++ W3D4/n_1_buster/app/models/album.rb
@@ -0,0 +1,12 @@
+require 'annotate'
+class Album < ApplicationRecord
+  belongs_to :artist,
+    class_name: 'Artist',
+    foreign_key: :artist_id,
+    primary_key: :id
+
+  has_many :tracks,
+    class_name: 'Track',
+    foreign_key: :album_id,
+    primary_key: :id
+end
