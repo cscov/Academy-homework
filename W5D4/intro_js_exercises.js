c605cac94b299cd5403a064c5ebead1ac3bba1d7
cscov <carolyn.l.scoville@gmail.com>
2018-03-16 06:52:11 -0700

    Created setTimeout function

---
+++ W5D4/intro_js_exercises.js
@@ -0,0 +1,5 @@
+function madLib(verb, adjective, noun) {
+  console.log("We shall ${verb}.toUppercase() the ${adjective}.toUppercase() ${noun}.toUppercase()");
+};
+
+madlib('make', 'best', 'guac');
