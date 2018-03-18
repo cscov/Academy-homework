c827ebf8ef1d812007df3408d6b310f6d1f53a0e
cscov <carolyn.l.scoville@gmail.com>
2018-03-17 21:42:14 -0700

    Added Elephant class

--- W5D4/intro_js_exercises.js
+++ W5D4/intro_js_exercises.js
@@ -64,3 +64,25 @@ function sumOfNPrimes(n) {
     let titleArr = array.map(name => `Mx. ${name} Jingleheimer Schmidt`);
     callback(titleArr);
   }
+
+  function Elephant(name, height, tricks) {
+    this.name = name;
+    this.height = height;
+    this.tricks = tricks;
+  }
+
+  Elephant.prototype.trumpet = function () {
+    console.log(`${this.name} the elephant goes 'phrRRRRRRRRRRR!!!!!!!'`);
+  };
+
+  Elephant.prototype.grow = function() {
+    this.height += 12;
+  };
+
+  Elephant.prototype.addTrick = function(trick) {
+    this.tricks.push(trick);
+  };
+
+  Elephant.prototype.play = function () {
+    console.log(`${this.name} is ${this.tricks[Math.round(Math.random(this.tricks.length - 1))]}!`);
+  };
