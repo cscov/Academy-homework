cb6c61aa651cdf64c5d185678297b87befed2cf1
cscov <carolyn.l.scoville@gmail.com>
2018-03-21 07:00:17 -0700

    Added ajax request of weather data

--- W6D3/firstAjax/js/index.js
+++ W6D3/firstAjax/js/index.js
@@ -1,5 +1,13 @@
 console.log("Hello from the JavaScript console!");
 
-// Your AJAX request here
+$.ajax({
+  url: ('http://api.openweathermap.org/data/2.5/weather?q=new%20york,US&appid=bcb83c4b54aee8418983c2aff3073b3b'),
+  type: 'GET',
+  success: function(weatherData) {
 
-// Add another console log here, outside your AJAX request
+    console.log('Weather data is below');
+    console.log(weatherData);
+  }
+});
+
+console.log('Outside of the ajax request');
